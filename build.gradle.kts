d4d99fb251f98f42ca46c7914b926ea6dcddb4e7
Kurt Guenther <kurtwguenther@gmail.com>
2026-08-15 17:26:07 -0600

    add reckon release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,10 @@ plugins {
     `java-library`
     `maven-publish`
     kotlin("jvm") version libs.versions.kotlin.get()
-    id("pl.allegro.tech.build.axion-release") version "1.21.2"
 }
-version = scmVersion.version
 
 allprojects {
     group = "io.github.version-ops"
-    version = project.version
 
     repositories {
         mavenLocal()
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,16 @@ pluginManagement {
     }
 }
 
+plugins {
+    id("org.ajoberstar.reckon.settings") version "0.19.2"
+}
+
+extensions.configure<org.ajoberstar.reckon.gradle.ReckonExtension> {
+    setDefaultInferredScope("minor")
+    snapshots()
+    setScopeCalc(calcScopeFromProp().or(calcScopeFromCommitMessages()))
+    setStageCalc(calcStageFromProp())
+}
 
 fun ProjectDescriptor.applyGradleKtsBuildFileNames() {
     children.forEach { subproject ->
