303447040919414d29a5d460b50b7e37c119d8b4
Kurt Guenther <kurtwguenther@gmail.com>
2026-08-17 19:02:58 -0600

    add-dockka

--- kandy-core/kandy-core.gradle.kts
+++ kandy-core/kandy-core.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("jvm")
     `java-library`
     `maven-publish`
+    id("org.jetbrains.dokka")
 }
 
 dependencies {
@@ -14,6 +15,7 @@ dependencies {
 // Apply a specific Java toolchain to ease working on diffesrent environments.
 java {
     withSourcesJar()
+    withJavadocJar()
     toolchain {
         languageVersion = JavaLanguageVersion.of(17)
     }
@@ -24,10 +26,16 @@ tasks.named<Test>("test") {
     useJUnitPlatform()
 }
 
+tasks.named<Jar>("javadocJar") {
+    from(tasks.named("dokkaGeneratePublicationHtml"))
+}
+
+
 publishing {
     publications {
         create<MavenPublication>("maven") {
             from(components["java"])
+//            artifact(javadocJar)
         }
     }
 }
